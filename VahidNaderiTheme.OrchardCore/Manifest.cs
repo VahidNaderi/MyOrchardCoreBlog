27754257a10cce301dec0203c059b403daaca6cd
v.naderi <v.naderi@arsina.net>
2023-04-01 14:59:42 +0330

    Initial commit

---
+++ VahidNaderiTheme.OrchardCore/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "VahidNaderiTheme.OrchardCore",
+    Author = "Vahid Naderi",
+    Website = "https://vahidnaderi.com",
+    Version = "0.0.1",
+    Description = "My Blog Theme"
+)]
